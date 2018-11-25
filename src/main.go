92cace28956974a2d8e79146f356c7a198ce8d55
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-25 16:36:18 +0700

    [Added]: set filename via program arguments

--- src/main.go
+++ src/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"try-to-mock-io/readfile"
 )
 
 func main() {
+	var filename string
+	flag.StringVar(&filename, "file", "../test-io", "set filename")
+	flag.Parse()
+
 	r := readfile.ReadFile{
 		Reader: readfile.RealReader,
 	}
-	fmt.Println(r.ReadFileString("../test-io"))
+	fmt.Println(r.ReadFileString(filename))
 }
