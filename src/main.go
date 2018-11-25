ebcd99a0e3fe220fb13e564d7c7802dd30aeeaa1
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-25 15:23:01 +0700

    [Deleted]: clean src/main.go

--- src/main.go
+++ src/main.go
@@ -1,33 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-)
-
-type ReadFile struct {
-	Reader func() ([]byte, error)
-}
-
-func FakeIOReadFile() ([]byte, error) {
-	return []byte("hi kk!"), nil
-}
-
-func RealIOReadFile() ([]byte, error) {
-	rawBytes, err := ioutil.ReadFile("../test-io")
-	return rawBytes, err
-}
-
 func main() {
-	fakeReadFile := ReadFile{
-		Reader: FakeIOReadFile,
-	}
-	fakeRawBytes, _ := fakeReadFile.Reader()
-	fmt.Println(string(fakeRawBytes))
-
-	realReadFile := ReadFile{
-		Reader: RealIOReadFile,
-	}
-	realRawBytes, _ := realReadFile.Reader()
-	fmt.Println(string(realRawBytes))
 }
