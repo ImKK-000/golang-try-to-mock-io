19169c7f52bb4e50bb2b4ae0b94207d62ec1c135
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-25 15:02:51 +0700

    [Added]: add struct for test mock and real io

--- main.go
+++ main.go
@@ -5,16 +5,27 @@ import (
 	"io/ioutil"
 )
 
-func MockReadFile() string {
-	return "hi kk!"
+type ReadFile struct {
+	Reader func() (string, error)
 }
 
-func ReadFile() string {
-	rawBytes, _ := ioutil.ReadFile("test-io")
-	return string(rawBytes)
+func FakeIOReadFile() (string, error) {
+	return "hi kk!", nil
+}
+
+func RealIOReadFile() (string, error) {
+	rawBytes, err := ioutil.ReadFile("test-io")
+	return string(rawBytes), err
 }
 
 func main() {
-	fmt.Println(MockReadFile())
-	fmt.Println(ReadFile())
+	fakeReadFile := ReadFile{
+		Reader: FakeIOReadFile,
+	}
+	fmt.Println(fakeReadFile.Reader())
+
+	realReadFile := ReadFile{
+		Reader: RealIOReadFile,
+	}
+	fmt.Println(realReadFile.Reader())
 }
